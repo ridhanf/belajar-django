71b619e23bd7c84f8843ea314e630fe3df5c15d6
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2019-10-02 06:03:38 +0700

    Belajar Django #03

--- dj03_intro_project/mysite/urls.py
+++ dj03_intro_project/mysite/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+# method view
+def index(request):
+    return HttpResponse("Halo, dunia!")
+
+def about(request):
+    return HttpResponse("Ini about!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/'),
+    path('about/', about),
+    path('', index),
 ]
