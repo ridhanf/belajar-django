e721011f06fea202d21bddb1fea0e02ec73dd1a6
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2019-10-15 17:31:44 +0700

    Belajar Django #6 - Pengenalan App

---
+++ dj06_pengenalan_apps/mysite/urls.py
@@ -0,0 +1,14 @@
+# mysite url
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+from blog import views as blogViews
+from about import views as aboutViews
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/',blogViews.index),
+    path('about/',aboutViews.index),
+    path('',views.index),
+]
